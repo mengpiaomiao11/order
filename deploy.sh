2c60fec6b2c5530ca5706380361d9552aa5d94ca
mengpiaomiao11 <525098542@qq.com>
2019-04-15 22:00:45 +0800

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -32,9 +32,9 @@ rm -f $TOMCAT_APP_PATH/webapps/ROOT.war
 rm -f $TOMCAT_APP_PATH/webapps/order.war
 
 # 复制新的工程
-cp -r $PROJ_PATH/order/target/order.war ./$TOMCAT_APP_PATH/webapps/
+cp -r $PROJ_PATH/order/target/order.war $TOMCAT_APP_PATH/webapps
 
-cd $TOMCAT_APP_PATH/webapps/
+cd $TOMCAT_APP_PATH/webapps
 mv order.war ROOT.war
 
 # 启动Tomcat
